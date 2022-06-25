03b78312ddd5ec2cb8813200e4cdec43da51581a
w35573 <prathamesh.redij@spit.ac.in>
2022-06-25 17:06:58 +0530

    fixed geolocation bugs

--- app.js
+++ app.js
@@ -33,71 +33,10 @@ let weather = {
         wind.textContent = `Wind Speed: ${speed} km/h`;
         loader.classList.remove('loading');
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
     }
 }
-
-
-// let geocode = {
-//     reverseGeocode: function (latitude, longitude) {
-//         let api_key = '4db2525097c645f8be5db0388d6f3d5f';
-
-//         let api_url = 'https://api.opencagedata.com/geocode/v1/json'
-
-//         let request_url = api_url
-//             + '?'
-//             + 'key=' + api_key
-//             + '&q=' + encodeURIComponent(latitude + ',' + longitude)
-//             + '&pretty=1'
-//             + '&no_annotations=1';
-
-//         // see full list of required and optional parameters:
-//         // https://opencagedata.com/api#forward
-
-//         let request = new XMLHttpRequest();
-//         request.open('GET', request_url, true);
-
-//         request.onload = function () {
-//             // see full list of possible response codes:
-//             // https://opencagedata.com/api#codes
-
-//             if (request.status === 200) {
-//                 // Success!
-//                 let data = JSON.parse(request.responseText);
-//                 weather.fetchWeather(data.results[0].components.city);
-//                 console.log(data.results[0].components.city)
-
-//             } else if (request.status <= 500) {
-//                 // We reached our target server, but it returned an error
-
-//                 console.log("unable to geocode! Response code: " + request.status);
-//                 let data = JSON.parse(request.responseText);
-//                 console.log('error msg: ' + data.status.message);
-//             } else {
-//                 console.log("server error");
-//             }
-//         };
-
-//         request.onerror = function () {
-//             // There was a connection error of some sort
-//             console.log("unable to connect to server");
-//         };
-
-//         request.send();  // make the request
-//     },
-
-//     getLocation: function () {
-//         function success(data) {
-//             geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
-//         }
-
-//         if (navigator.geolocation) {
-//             navigator.geolocation.getCurrentPosition(success, console.error);
-//         }
-//         else {
-//             weather.fetchWeather('Mumbai');
-//         }
-//     }
-// };
 
 
 searchBtn.addEventListener('click', () => {
@@ -128,9 +67,19 @@ function success(pos) {
 
 async function getCurrentCity(latitude, longitude) {
     try {
-        const res = await axios.get(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${weather.apiKey}`);
-        console.log(res.data[0].name);
-        weather.fetchWeather(res.data[0].name);
+        let api_key = '4db2525097c645f8be5db0388d6f3d5f';
+        let api_url = 'https://api.opencagedata.com/geocode/v1/json'
+        let request_url = api_url
+            + '?'
+            + 'key=' + api_key
+            + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+            + '&pretty=1'
+            + '&no_annotations=1';
+
+        const res = await axios.get(request_url);
+        console.log(res.data.results[0].components.town);
+        weather.fetchWeather(res.data.results[0].components.town);
+
     } catch (error) {
         console.log(error);
     }
